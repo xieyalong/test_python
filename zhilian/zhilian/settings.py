7708a410db59b06a426af505e6f2593af827699e
xieyalong <123456>
2019-09-12 15:38:07 +0800

    ----

--- zhilian/zhilian/settings.py
+++ zhilian/zhilian/settings.py
@@ -27,7 +27,8 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+#下载延迟，模拟人在操作
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
