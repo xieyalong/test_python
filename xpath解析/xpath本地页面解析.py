cc38cd4bf795cd1362a186a6e9283a7c1238cccb
xieyalong <123456>
2019-09-12 10:44:22 +0800

    ----

--- xpath解析/xpath本地页面解析.py
+++ xpath解析/xpath本地页面解析.py
@@ -8,9 +8,11 @@ parser = etree.HTMLParser(encoding='utf-8')
 html = etree.parse(r'html\xpath本地纯网页.html', parser=parser)
 print(html)
 
+list=html.xpath('//span')
+print('当前节点下所有span,各个层级=',len(list))
 
-print('当前节点下所有span,各个层级=',len(html.xpath('//span')))
-print('当前节点下class="p2 p3"所有span=',len(html.xpath('//span[@class="p2 p3"]')))
+list=html.xpath('//span[@class="p2 p3"]')
+print('当前节点下class="p2 p3"所有span=',len(list))
 
 #只获取text内容，span里面有子节点不会获取
 list=html.xpath('//span[@class="p2 p3"]/text()');
@@ -22,7 +24,12 @@ print('根据一个属性获取href值=',list)
 list=html.xpath('//a[contains(@title,"赵匡胤") and contains(@id,"_id") and contains(@about,"_about")]/@href')
 print('根据多个属性获取href值=',list)
 
+list=html.xpath('//div[@id="_div3"]/span/text()')
+print('获取div子元素span的内容=',list)
 
+#p标签不起作用，如果换做span就起作用
+list=html.xpath('//p[@class="p1"]/div/text()')
+print('====',list)
 
 # with open(r'aaaa.html','w',encoding='utf-8') as fw:
 #     fw.write(str(html))
